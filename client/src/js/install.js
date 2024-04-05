3a81444fc1e424e955c5f8a490f14e4305d69365
Chris Stevenson <chrislstevenson@gmail.com>
2024-04-05 16:00:38 -0400

    Fix install buttons for PWA
    - install button is off by default
    - it is made visible only if the PWA can be installed

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,26 +1,41 @@
 const installBtnEl = document.getElementById('buttonInstall');
 
+// global variable to show install prompt
+// (or detect if app already installed)
+let promptEvent = null;
+
+// hide the install button unless it needs to be shown
+installBtnEl.style.display = 'none';
+
 // Logic for installing the PWA
-// Event handler to the `beforeinstallprompt` event
+// Event handler to the `beforeinstallprompt` event,
+// which fires if this is an installable PWA
 window.addEventListener('beforeinstallprompt', (event) => {
-  // Store the triggered events
-  window.deferredPrompt = event;
-  // Remove the hidden class from the button.
-  installBtnEl.classList.toggle('hidden', false);
+  // Store the triggered events as a global variable
+  promptEvent = event;
+  // Show the install button
+  installBtnEl.style.display = 'inline';
 });
 
 // Event handler on the `butInstall` element
 installBtnEl.addEventListener('click', async () => {
-  if (!promptEvent) return;
-  // Show prompt
+  if (!promptEvent) {
+    alert('App already installed!');
+    installBtnEl.style.display = 'none';
+    return;
+  }
+
+  // Show prompt and possibly install PWA
+  // If the user installs, the 'appinstalled' event will fire
+  // and the install button will disappear
   promptEvent.prompt();
-  // Reset the deferred prompt variable, it can only be used once.
-  window.deferredPrompt = null;
-  installBtnEl.classList.toggle('hidden', true);
 });
 
-// Event handler for the `appinstalled` event
+// Event handler for the `appinstalled` event, whic
+// fires if the user actually installs the PWA
 window.addEventListener('appinstalled', (event) => {
   // Clear prompt
-  window.deferredPrompt = null;
+  promptEvent = null;
+  // make sure install button is hidden
+  installBtnEl.style.display = 'none';
 });
